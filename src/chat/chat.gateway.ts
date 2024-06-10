b8e3d05319b5c06ab2e7c8c1135c324987dc1a86
mehtapcelik876 <mehtap.celik@fmssolutions.com>
2024-06-10 11:55:48 +0300

    completed nest js web socket with rooms

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,37 @@
+import { Logger } from '@nestjs/common';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ namespace: '/chat' })
+export class ChatGateway {
+  @WebSocketServer() wss: Server;
+  private logger: Logger = new Logger('ChatGateway');
+
+  afterInit(server: any) {
+    this.logger.log('Initialized!');
+  }
+
+  @SubscribeMessage('chatToServer')
+  handleMessage(
+    client: Socket,
+    message: { sender: string; room: string; message: string },
+  ) {
+    this.wss.to(message.room).emit('chatToClient', message);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, room: string) {
+    client.join(room);
+    client.emit('joinedRoom', room);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleLeaveRoom(client: Socket, room: string) {
+    client.leave(room);
+    client.emit('leftRoom', room);
+  }
+}
